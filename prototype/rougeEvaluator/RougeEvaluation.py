4563c2c673afbb3e122d8ee64d00ec8c9ea7e9eb
Teemu H <herttuateemu@gmail.com>
2020-11-08 23:40:00 +0000

    Docstrings and cleaning up
    Cleaned up the repository
    Updated the comments and docstrings

--- prototype/rougeEvaluator/RougeEvaluation.py
+++ prototype/rougeEvaluator/RougeEvaluation.py
@@ -1,22 +1,23 @@
-from textblob import TextBlob #Textblob is used for creating bigrams and trigrams
+from textblob import TextBlob
 import re
 
 class RougeEvaluation():
-
     def __init__(self):
         pass
 
-    #N-grams are created from the given (human created) reference text. Those N-grams are then searched from the summary.
-    #The function returns the amount of N-grams and the amount of words in the summary text.
     def ngram_creation_and_counting(self, reference_text, summary_text, grams):
-        #Create tuple lists of the reference text and the summary
+        """
+        N-grams are created from the given (human written) reference text. Those N-grams are then searched from the summary.
+        The method returns the amount of N-grams and the amount of words in the summary text.
+        Creates tuple lists of the reference text and the summary.
+        Then compares the reference wordlist tuples and compares them to the summary tuples
+        """
         ngram_referencetuples = TextBlob(reference_text).ngrams(grams)
         ngram_summarytuples = TextBlob(summary_text).ngrams(grams)
 
         summary_counter = 0
         match_counter = 0
 
-        #Loop through all the tuples in the reference wordlist and compare them to the tuples in the summary tuples wordlist
         for _ in ngram_summarytuples:
             reference_counter = 0
             for __ in ngram_referencetuples:
@@ -29,17 +30,24 @@ class RougeEvaluation():
 
         return match_counter, ngram_count
 
-    #Rouge recall is calculated based on the matches and the bigram count and returned
     def rouge_recall(self, match_amount, ngram_amount):
+        """
+        Calculates the Rouge-N precision based on the matches and the bigram count
+        """
         recall = match_amount / ngram_amount
         return str(recall)
 
-    #Rouge precision is calculated and returned
     def rouge_precision(self, match_amount, text_word_amount):
+        """
+        Calculates the Rouge-N precision
+        """
         precision = match_amount / text_word_amount
         return str(precision)
 
     def rouge_evaluations(self, summary,reference):
+        """
+        Uses the methods to calculate recall and precision values for both Rouge-2 and Rouge-3
+        """
         rouge2results = self.ngram_creation_and_counting(reference,summary,2)
         rouge3results = self.ngram_creation_and_counting(reference,summary,3)
         summary_wordcount = TextBlob(summary)
--- prototype/summarizers/summarizer.py
+++ prototype/summarizers/summarizer.py
@@ -3,21 +3,33 @@ from pytldr.summarize.lsa import LsaOzsoy, LsaSteinberger
 from pytldr.summarize import RelevanceSummarizer
 
 def textRank(text, refSentences=5):
+    """
+    Loads the Text rank summarizer class and procudes a summary using it
+    """
     TextRankSum = TextRankSummarizer()
     summary_TextRank = TextRankSum.summarize(text, length=refSentences)
     return summary_TextRank
 
 def LSA_O(text, refSentences=5):
+    """
+    Loads the LSA Ozsoy summarizer class and procudes a summary using it
+    """
     LSAOSum = LsaOzsoy()
     o_summary = LSAOSum.summarize(text, topics=4, length=refSentences, binary_matrix=True, topic_sigma_threshold=0.5)
     return o_summary
 
 def LSA_S(text, refSentences=5):
+    """
+    Loads the LSA Steinberg summarizer class and procudes a summary using it
+    """
     LSASSum = LsaSteinberger()
     s_summary = LSASSum.summarize(text, topics=4, length=refSentences, binary_matrix=True, topic_sigma_threshold=0.5)
     return s_summary
 
 def relevance(text, refSentences=5):
+    """
+    Loads the Relevance based summarizer class and procudes a summary using it
+    """
     RelevanceSum = RelevanceSummarizer()
     summary_relevance = RelevanceSum.summarize(text, length=refSentences, binary_matrix=True)
     return summary_relevance
